6001424e06a00ce2302e1283f28d805901bfb271
Suraj <suraj.zinzuwadia@gmail.com>
2019-03-15 01:54:31 +0530

    Change in h1 tag and h2 tag

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default',function(){
 	console.log("Finally created gulp task..")
@@ -15,7 +16,7 @@ gulp.task('html',function(){
 
 gulp.task('styles',function(){
 	return gulp.src('./app/styles/styles.css')
-	.pipe(postcss([cssvars ,nested , autoprefixer]))
+	.pipe(postcss([cssImport , cssvars ,nested , autoprefixer]))
 	.pipe(gulp.dest('./app/temp/styles'));  
 });
 
