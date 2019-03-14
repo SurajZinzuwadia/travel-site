24993bd5beba3badf08e48bdc939403334510511
Suraj <suraj.zinzuwadia@gmail.com>
2019-03-14 23:50:57 +0530

    used pipeing in style.css

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default',function(){
+	console.log("Finally created gulp task..")
+});
+
+gulp.task('html',function(){
+	console.log("Imagine html here.")
+});
+
+gulp.task('styles',function(){
+	return gulp.src('./app/styles/styles.css')
+	.pipe(postcss([cssvars ,nested , autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));  
+});
+
+gulp.task('watch',function(){
+	watch('./app/index.html',function(){
+		gulp.start('html');
+	});
+	watch('./app/styles/**/*.css',function(){
+		gulp.start('styles');
+	});
+}); 
