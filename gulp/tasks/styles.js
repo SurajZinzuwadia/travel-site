361f2a3f488f80fa0e983e7a75b04b87b2708e2f
Suraj <suraj.zinzuwadia@gmail.com>
2019-03-16 01:24:24 +0530

    completed large hero block and blocks

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,9 +4,14 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles',function(){
 	return gulp.src('./app/styles/styles.css')
-	.pipe(postcss([cssImport , cssvars ,nested , autoprefixer]))
+	.pipe(postcss([cssImport , mixins ,cssvars ,nested , autoprefixer]))
+	.on('error',function(errorInfo){
+		console.log(errorInfo.toString());
+		this.emit('end');
+	})
 	.pipe(gulp.dest('./app/temp/styles'));  
 });
